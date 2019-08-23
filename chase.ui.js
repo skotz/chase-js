676debd004435050b86c8e1cd91b43cb84921777
Scott Clayton <scottclayton84@gmail.com>
2019-08-22 20:14:34 -0500

    Updated math and colors

--- chase.ui.js
+++ chase.ui.js
@@ -15,7 +15,19 @@ var settings = {
 		return this.hexHeight() + (3 * this.hexSpace - this.hexWidth) / (2 * Math.sqrt(3));
 	},
 	/* Colors */
-	hexColor: 0x777777
+	colorEmpty: 0x777777,
+	colorRed: 0xFF3333,
+	colorBlue: 0x4499FF,
+	hexTexture: function(points, color) {
+		const graphics = new PIXI.Graphics();
+		graphics.beginFill(color);
+		graphics.lineStyle(0);
+		graphics.drawPolygon(points);
+		graphics.endFill();
+		var texture = PIXI.RenderTexture.create(graphics.width, graphics.height);
+		app.renderer.render(graphics, texture);
+		return texture;
+	}
 };
 
 // Initialize app
@@ -43,10 +55,9 @@ var createHex = function (hex) {
 		x += insetOffset;
 	}
 
-	const pointFactTop = 0.2828947;
-	const pointFactMid = 0.8618421;
-	const pointFactBottom = 1.1447368;
-	const widthToHeight = 1.1547005;
+	const pointFactTop = 1 / (2 * Math.sqrt(3));
+	const pointFactMid = 3 / (2 * Math.sqrt(3));
+	const pointFactBottom = 2 / Math.sqrt(3);
 
 	const points = [
 		0, settings.hexWidth * pointFactTop,
@@ -57,14 +68,7 @@ var createHex = function (hex) {
 		settings.hexWidth / 2.0, 0
 	];
 
-    const graphics = new PIXI.Graphics();
-    graphics.beginFill(settings.hexColor);
-    graphics.lineStyle(0);
-    graphics.drawPolygon(points);
-    graphics.endFill();
-
-    const texture = PIXI.RenderTexture.create(graphics.width, graphics.height);
-    app.renderer.render(graphics, texture);
+    const texture = settings.hexTexture(points, settings.colorEmpty);
 
     const sprite = new PIXI.Sprite(texture);
     sprite.x = x;
@@ -80,18 +84,25 @@ var createHex = function (hex) {
 		this.alpha = 1.0;
     };
     
-    return sprite;
+    return {
+		color: settings.colorEmpty,
+		points: points,
+		sprite: sprite
+	};
 }
 
+var hexTiles = [];
 var initializeBoard = function (hex) {
 	// Clear the stage
+	hexTiles = [];
 	while (app.stage.children[0]) { 
 		app.stage.removeChild(app.stage.children[0]);
 	}
 	// Create the hex grid
 	for (var i = 0; i < 81; i++) {
 		var hex = createHex({ id: i });
-		app.stage.addChild(hex);
+		hexTiles.push(hex);
+		app.stage.addChild(hex.sprite);
 	}
 }
 
@@ -108,8 +119,6 @@ function resize() {
 	// const newWidthBasedOnHeight = (2 * Math.sqrt(3) * parent.clientHeight - (4 * Math.sqrt(3) + 24) * settings.hexSpace) / 30.0;
 	const newWidthBasedOnHeight = ((12 + 2 * Math.sqrt(3)) * settings.hexSpace - Math.sqrt(3) * parent.clientHeight) / -14.0;
 	
-console.log(newWidthBasedOnHeight)
-
 	// Resise to whatever fits best
 	settings.hexWidth = Math.min(newWidthBasedOnWidth, newWidthBasedOnHeight);
 
@@ -118,6 +127,31 @@ console.log(newWidthBasedOnHeight)
 window.addEventListener('resize', resize);
 resize();
 
+CHASE.AI.NewGame();
+
 // Game loop
 app.ticker.add((delta) => {
+	var position = CHASE.AI.Board;
+	var winner = CHASE.AI.Position.getWinner(CHASE.AI.Board);
+	
+	// Refresh the values of the pieces on the board
+	for (var i = 0; i < 81; i++) {
+		if (i != 40) {
+			var value = position.tiles[i] != 0 ? Math.abs(position.tiles[i]) : "";
+			if (position.tiles[i] > 0) {
+				if (hexTiles[i].color != settings.colorBlue) {
+					hexTiles[i].color = settings.colorBlue;
+					hexTiles[i].sprite.texture = settings.hexTexture(hexTiles[i].points, settings.colorBlue);
+				}
+			} else if (position.tiles[i] < 0) {
+				if (hexTiles[i].color != settings.colorRed) {
+					hexTiles[i].color = settings.colorRed;
+					hexTiles[i].sprite.texture = settings.hexTexture(hexTiles[i].points, settings.colorRed);
+				}
+			} else if (hexTiles[i].color != settings.colorEmpty) {
+				hexTiles[i].color = settings.colorEmpty;
+				hexTiles[i].sprite.texture = settings.hexTexture(hexTiles[i].points, settings.colorEmpty);
+			}
+		}
+	}
 });
